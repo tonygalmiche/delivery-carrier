4c5b836a4cc30abe8a4d845dff89adad3add4fe0
Raph <raph@poir.es>
2016-12-02 14:13:49 +0100

    Add delivery_roulier_laposte
    != from v8:
    - changes on data: delivery.carrier is now a product
    - remove config
    - add keychain dependency

---
+++ delivery_roulier_laposte/models/__init__.py
@@ -0,0 +1,5 @@
+from . import company
+from . import stock
+from . import delivery
+from . import stock_package
+from . import keychain
---
+++ delivery_roulier_laposte/models/keychain.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+#  @author Raphael Reverdy <raphael.reverdy@akretion.com>
+#          David BEAL <david.beal@akretion.com>
+#          Sébastien BEAU
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+import logging
+from openerp import models, fields
+
+_logger = logging.getLogger(__name__)
+
+
+class AccountProduct(models.Model):
+    _inherit = 'keychain.account'
+
+    namespace = fields.Selection(
+        selection_add=[('roulier_laposte', 'Laposte')])
+
+    def _roulier_laposte_init_data(self):
+        return {
+            "codeAgence": "",
+        }
+
+    def _roulier_laposte_validate_data(self, data):
+        # on aurait pu utiliser Cerberus ici
+        return 'codeAgence' in data
